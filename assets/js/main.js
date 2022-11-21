f4cfa59fc7f9729a582bba0410c0740e1f9d6135
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-21 14:45:23 +0100

    Project and bonus completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
 var app = new Vue({
   el: "#root",
-  data: {},
+  data: {
+    indiceMail: [], //Array vuoto dove aggiungerò le mail
+  },
   mounted() {},
   beforeUpdate() {},
-  methods: {},
+  methods: {
+    //Funzione che mi genera una mail random e la pusho all'interno dell'array vuoto
+    creaMail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          //console.log(response.data.response);
+          let responseData = response.data.response;
+          this.indiceMail.push(responseData);
+          console.log(this.indiceMail);
+        });
+    },
+
+    //Funzione che cicla la funzione creaMail() per generale più mail diverse
+
+    cicle() {
+      for (let i = 0; i < 10; i++) {
+        this.creaMail();
+      }
+    },
+  },
 });
